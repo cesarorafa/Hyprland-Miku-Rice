5d621f5b21138889746c1e22cbbf52ab7a3be2cd
rafa <rafacs1121@gmail.com>
2025-10-03 22:29:53 -0300

    install.sh terminado

--- dotfiles/Install.sh
+++ dotfiles/Install.sh
@@ -1,8 +1,29 @@
-read -p " you want to install the packages? (y/n) " installpackages
+#!/bin/bash
 
-if [["$installpackages" =~ ^[Yy]$]]; then
- yay -S waybar fastfetch cava kitty starship swww
+read -p "You want to install the packages? (y/n) " installpackages
+
+if [[ "$installpackages" =~ ^[Yy]$ ]]; then
+    yay -S waybar fastfetch cava kitty starship swww
 else
- echo "Install script canceled"
+    echo "Install script canceled"
+    exit 0
 fi
+
+read -p "Do you want to copy the .config files to your .config? (y/n) " copyconfig
+
+if [[ "$copyconfig" =~ ^[Yy]$ ]]; then
+    mkdir -p ~/.config
+    cp ~/dotfiles/.config/* ~/.config/
+    echo "Configs copied!"
 else
+    echo "Config copy canceled"
+fi
+read -p "Do you want to copy the .bashrc? (y/n) " copybashrc
+
+if [[ "$copybashrc" =~ ^[Yy]$ ]]; then
+    cp ~/dotfiles/.bashrc ~/.bashrc
+    echo "Sourcing new .bashrc..."
+    source ~/.bashrc
+else
+    echo ".bashrc copy canceled."
+fi
